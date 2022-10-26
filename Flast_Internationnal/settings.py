e0564793dd7e3f34bcabc67bd91ec4a8a5130193
kabreaziz60 <kabreaziz60@gmail.com>
2022-10-26 15:50:25 +0000

    Start deploit

--- Flast_Internationnal/settings.py
+++ Flast_Internationnal/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoise.Mdiddleware',
+    'whitenoise.middleware.WhiteNoise.Mididdleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
